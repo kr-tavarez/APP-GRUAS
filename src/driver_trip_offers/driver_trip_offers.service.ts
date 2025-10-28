9d5f7f840b5b2e857de674707e45ea8a04c9096e
kr-tavarez <56976011+kr-tavarez@users.noreply.github.com>
2025-10-28 12:10:01 -0300

    Refactor DriverTripOffersService methods
    Refactor create method to be asynchronous and add findByClientRequest method.

--- src/driver_trip_offers/driver_trip_offers.service.ts
+++ src/driver_trip_offers/driver_trip_offers.service.ts
@@ -1,17 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { DriverTripOffers } from './driver_trip_offers.entity';
 import { Repository } from 'typeorm';
+import { DriverTripOffers } from './driver_trip_offers.entity';
 import { CreateDriverTripOffersDto } from './dto/create_driver_trip_offers.dto';
 
 @Injectable()
 export class DriverTripOffersService {
 
-    constructor(@InjectRepository(DriverTripOffers) private driverTripOffersRepository: Repository<DriverTripOffers>) {}
+  constructor(
+    @InjectRepository(DriverTripOffers)
+    private readonly driverTripOffersRepository: Repository<DriverTripOffers>,
+  ) {}
 
-    create(driverTripOffer: CreateDriverTripOffersDto) {
-        const newData = this.driverTripOffersRepository.create(driverTripOffer);
-        return this.driverTripOffersRepository.save(newData);
-    }_
+  async create(driverTripOffer: CreateDriverTripOffersDto) {
+    const newData = this.driverTripOffersRepository.create(driverTripOffer);
+    return await this.driverTripOffersRepository.save(newData);
+  }
 
+  // ✅ Método que faltaba
+  async findByClientRequest(id_client_request: number) {
+    return await this.driverTripOffersRepository.find({
+      where: { id_client_request },
+    });
+  }
 }
+
