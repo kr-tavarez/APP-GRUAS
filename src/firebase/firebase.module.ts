19a2afd1f854467112db25df0376925d7cad01b7
kr-tavarez <56976011+kr-tavarez@users.noreply.github.com>
2025-10-28 11:59:09 -0300

    Refactor Firebase app initialization logic
    Refactor Firebase initialization to check for existing apps before initializing. Update private key handling to use optional chaining.

--- src/firebase/firebase.module.ts
+++ src/firebase/firebase.module.ts
@@ -7,11 +7,12 @@ const firebaseProvider = {
   provide: 'FIREBASE_APP',
   inject: [ConfigService],
   useFactory: (configService: ConfigService) => {
+    
     const firebaseConfig = {
       type: configService.get<string>('TYPE'),
       project_id: configService.get<string>('PROJECT_ID'),
       private_key_id: configService.get<string>('PRIVATE_KEY_ID'),
-      private_key: configService.get<string>('PRIVATE_KEY').replace(/\\n/gm, "\n"),
+      private_key: configService.get<string>('PRIVATE_KEY')?.replace(/\\n/g, '\n'),
       client_email: configService.get<string>('CLIENT_EMAIL'),
       client_id: configService.get<string>('CLIENT_ID'),
       auth_uri: configService.get<string>('AUTH_URI'),
@@ -21,16 +22,22 @@ const firebaseProvider = {
       universe_domain: configService.get<string>('UNIVERSAL_DOMAIN'),
     } as admin.ServiceAccount;
 
-    return admin.initializeApp({
-      credential: admin.credential.cert(firebaseConfig),
-    //   databaseURL: `https://${firebaseConfig.projectId}.firebaseio.com`,
-    //   storageBucket: `${firebaseConfig.projectId}.appspot.com`,
-    });
+    if (!admin.apps.length) {
+      return admin.initializeApp({
+        credential: admin.credential.cert(firebaseConfig),
+      });
+    }
+
+    return admin.app();
   },
 };
 
 @Module({
-  imports: [ConfigModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,       // ✅ para no tener que importar ConfigModule en otros módulos
+    }),
+  ],
   providers: [firebaseProvider, FirebaseRepository],
   exports: [FirebaseRepository],
 })
