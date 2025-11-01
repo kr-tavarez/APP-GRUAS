c3930ad8d9f03944d6745287beada768b7aeb231
kr-tavarez <56976011+kr-tavarez@users.noreply.github.com>
2025-11-01 13:15:20 -0300

    Implement home endpoint with status message
    Replace the getHello method with a home method that returns a status message.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
 
-  // 'RAIZ -> /'
-  // @Get()
-  // getHello(): string {
-  //   return this.appService.getHello();
-  // }
+  @Get()
+  home() {
+    return {
+      status: 'running',
+      message: 'Backend GruApp activo 🚀'
+    };
+  }
 }
